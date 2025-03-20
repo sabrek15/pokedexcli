f19b7527178720960b585a08360bbcc4718198e3
sabrek15 <reddy.ksb007@gmail.com>
2025-03-20 17:16:09 +0530

    updated main file

--- main.go
+++ main.go
@@ -1,35 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"bufio"
+	"time"
+	
+	"github.com/sabrek15/pokedexcli/internal/pokeapi"
 )
 
 func main() {
-	cfg := &config{}
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanned := scanner.Scan()
-		if !scanned {
-			break
-		}
-		input := scanner.Text()
-
-		if input == "" {
-			continue
-		}
-
-		words := cleanInput(input)
-
-		if cmd, exists := commands[words[0]]; exists {
-			err := cmd.callback(cfg)
-			if err != nil {
-				fmt.Printf("Error: %v\n", err)
-			}
-		} else {
-			fmt.Println("unknown Command")
-		}
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
 	}
+	startrepl(cfg)
 }
