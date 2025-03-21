52749bd8b018bf9c2957c208bc09f71493f7314a
sabrek15 <reddy.ksb007@gmail.com>
2025-03-21 11:38:32 +0530

    updated the location_list.go as per pokecache logic

--- internal/pokeapi/location_list.go
+++ internal/pokeapi/location_list.go
@@ -13,6 +13,15 @@ func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 		url = *pageURL
 	}
 
+	if val, ok := c.cache.Get(url); ok {
+		locationResp := RespShallowLocations{}
+		err := json.Unmarshal(val, &locationResp)
+		if err != nil {
+			return RespShallowLocations{}, err
+		}
+		return locationResp, nil
+	}
+
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return RespShallowLocations{}, err
@@ -34,6 +43,8 @@ func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 	if err != nil {
 		return RespShallowLocations{}, err
 	}
+
+	c.cache.Add(url, dat)
 
 	return locationsResp, nil
 }
